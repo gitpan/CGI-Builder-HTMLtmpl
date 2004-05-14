c2b1f271082853cfbdeed9d106279a7ac7317477
Domizio Demichelis <domizio@cpan.org>
2004-05-14 20:03:38 +0000

    Import of DOMIZIO/CGI-Builder-HTMLtmpl-1.02 from CPAN.
    gitpan-cpan-distribution: CGI-Builder-HTMLtmpl
    gitpan-cpan-version:      1.02
    gitpan-cpan-path:         DOMIZIO/CGI-Builder-HTMLtmpl-1.02.tar.gz
    gitpan-cpan-author:       DOMIZIO
    gitpan-cpan-maturity:     released

--- lib/CGI/Builder/HTMLtmpl.pm
+++ lib/CGI/Builder/HTMLtmpl.pm
@@ -1,5 +1,8 @@
 package CGI::Builder::HTMLtmpl ;
-$VERSION = 1.01 ;
+$VERSION = 1.02 ;
+
+# This file uses the "Perlish" coding style
+# please read http://perl.4pro.net/perlish_coding_style.html
 
 ; use strict
 ; use HTML::Template
@@ -42,7 +45,7 @@ __END__
 
 CGI::Builder::HTMLtmpl - CGI::Builder and HTML::Template integration
 
-=head1 VERSION 1.01
+=head1 VERSION 1.02
 
 To have the complete list of all the extensions of the CBF, see L<CGI::Builder/"Extensions List">
 
@@ -57,7 +60,7 @@ To have the complete list of all the extensions of the CBF, see L<CGI::Builder/"
 
 =item CPAN
 
-    perl -MCPAN -e 'install Apache::CGI::Builder'
+    perl -MCPAN -e 'install CGI::Builder::HTMLtmpl'
 
 You have also the possibility to use the Bundle to install all the extensions and prerequisites of the CBF in just one step. Please, notice that the Bundle will install A LOT of modules that you might not need, so use it specially if you want to extensively try the CBF.
 
@@ -71,6 +74,8 @@ From the directory where this file is located, type:
     make
     make test
     make install
+
+B<Note>: The installation of this module runs an automatic version check connection which will warn you in case a newer version is available: please don't use old versions, because I can give you full support only for current versions. Besides, since CPAN does not provide any download statistic to the authors, this check allows me also to keep my own installation counter. Version checking is transparent to regular users, while CPAN testers should skip it by running the Makefile.PL with NO_VERSION_CHECK=1.
 
 =back
 
